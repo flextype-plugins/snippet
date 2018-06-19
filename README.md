9e53d3b9376b2b9bfb0513fd93df977cc6564ae0
Awilum <awilum@msn.com>
2018-06-20 02:28:38 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# snippet
-Snippet plugin provides a basic way to work with code snippets.
+# Snippet Plugin for [Flextype](http://flextype.org/)
+![version](https://img.shields.io/badge/version-1.0.0-brightgreen.svg?style=flat-square)
+![Flextype](https://img.shields.io/badge/Flextype-0.x-green.svg?style=flat-square)
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+
+Snippet plugin provides a basic way to work with code snippets and saved with `.php` extension in the folder `/site/snippets/`
+
+## Installation
+1. Unzip plugin to the folder `/site/plugins/`
+2. Go to `/site/config/site.yaml` and add plugin name to plugins section.
+3. Save your changes.
+
+Example:
+```
+plugins:
+  - snippet
+```
+
+## Usage in page content
+
+```
+[snippet name=snippet-name]
+```
+
+## Usage in the template
+
+Define Flextype namespace in the template if it is not defined yet.
+```
+<?php namespace Flextype; ?>
+```
+
+Display block content
+```
+<?php echo Snippet::get('snippet-name'); ?>
+```
+
+## Settings
+
+```yaml
+enabled: true # or `false` to disable the plugin
+```
+
+## License
+See [LICENSE](https://github.com/flextype-plugins/snippet/blob/master/LICENSE)
---
+++ snippet.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ *
+ * Flextype Snippet Plugin
+ *
+ * @author Romanenko Sergey / Awilum <awilum@yandex.ru>
+ * @link http://flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+use Flextype\Component\{Event\Event, Filesystem\Filesystem};
+
+// Event: onShortcodesInitialized
+Event::addListener('onShortcodesInitialized', function () {
+
+    // Shortcode: [snippet name=snippet-name]
+    Content::shortcode()->addHandler('snippet', function(ShortcodeInterface $s) {
+        return Snippet::get($s->getParameter('name'));
+    });
+});
+
+class Snippet
+{
+    /**
+     * Get snippet
+     *
+     * Snippet::get('snippet-name');
+     *
+     * @access public
+     * @param  string  $snippet_name  Snippet name
+     * @return void
+     */
+    public static function get($snippet_name) : void
+    {
+        $snippet_path = PATH['site'] . '/snippets/' . $snippet_name . '.php';
+
+        if (Filesystem::fileExists($snippet_path)) {
+            include $snippet_path;
+        } else {
+            throw new \RuntimeException("Snippet {$snippet_path} does not exist.");
+        }
+    }
+}
